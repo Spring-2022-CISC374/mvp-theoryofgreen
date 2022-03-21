f4234c92434a7855e746387a96aecaa783814a90
Allison Z <alzig@udel.edu>
2022-03-21 18:35:01 -0400

    Broke up character spritesheet, added idle animation and walking animations

--- Scene2.js
+++ Scene2.js
@@ -7,6 +7,7 @@ class Scene2 extends Phaser.Scene {
 
         this.player = this.physics.add.sprite(300, 300, "player");
         this.player.setScale(2);
+        this.player.play("idle");
         this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.player.setCollideWorldBounds(true);
 
@@ -20,22 +21,28 @@ class Scene2 extends Phaser.Scene {
 
     movePlayerManager() {
         if(this.cursorKeys.left.isDown) {
+            this.player.play("walk_left", true);
             this.player.setVelocityX(-gameSettings.playerSpeed);
+            this.player.setVelocityY(0);
         }
         else if(this.cursorKeys.right.isDown) {
+            this.player.play("walk_right", true);
             this.player.setVelocityX(gameSettings.playerSpeed);
+            this.player.setVelocityY(0);
         }
-        else {
+        else if(this.cursorKeys.up.isDown) {
+            this.player.play("walk_up", true);
             this.player.setVelocityX(0);
-        }
-
-        if(this.cursorKeys.up.isDown) {
             this.player.setVelocityY(-gameSettings.playerSpeed);
         }
         else if(this.cursorKeys.down.isDown) {
+            this.player.play("walk_down", true);
+            this.player.setVelocityX(0);
             this.player.setVelocityY(gameSettings.playerSpeed);
         }
         else {
+            this.player.play("idle", true);
+            this.player.setVelocityX(0);
             this.player.setVelocityY(0);
         }
     }
