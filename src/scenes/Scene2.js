45d5e94c9b15727fa1e1ce98a7f500c1029502e9
cthacker-udel <cthacker@udel.edu>
2022-03-22 09:01:22 -0400

    [score-label] Configured tween animation to not play rather render horizontal rectangle immediately

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -6,14 +6,16 @@ export default class Scene2 extends Phaser.Scene {
     create() {
         this.score = 0;
         this.scoreLabelText = this.add.text(400, 250, `Score : ${this.score}`);
-        this.scoreLabelText.setOrigin(0.5, 0.5);
-        this.scoreLabelText.setColor('white');
+        this.scoreLabelText.setOrigin(0.5, 14);
+        this.scoreLabelText.depth = 100;
+        this.scoreLabelText.setColor('black');
 
         const score_label = this.add.rectangle(400, 50, 30, 200, 0xffffff, 1);
         this.physics.add.existing(score_label, true);
         this.tweens.add({
             targets: score_label,
-            angle: 90
+            angle: 90,
+            duration: 0
         });
 
         /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
