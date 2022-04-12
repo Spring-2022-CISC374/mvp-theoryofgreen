368c936154e51410cc2dc94ac284532ab4869932
Allison Z <alzig@udel.edu>
2022-04-12 01:20:48 -0400

    Implemented game timer that sends player to a Game Over screen when time runs out

--- src/Scenes/Scene1.js
+++ src/Scenes/Scene1.js
@@ -158,6 +158,7 @@ class Scene1 extends Phaser.Scene {
     this.isFire = false;
     this.fireCheck = 0;
     this.shelterCheck = 0;
+    this.timerDelay = 500000;
 
     this.add.text(20, 20, "Loading game...");
     this.scene.start("playGame", {
@@ -168,7 +169,8 @@ class Scene1 extends Phaser.Scene {
       "isShelter": this.isShelter,
       "isFire": this.isFire,
       "fireCheck": this.fireCheck,
-      "shelterCheck": this.shelterCheck
+      "shelterCheck": this.shelterCheck,
+      "timerDelay": this.timerDelay
     });
 
     // #region ANIMS
