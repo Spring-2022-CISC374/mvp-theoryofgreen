253362dadc00554fc42d8e26787740d8af56f083
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-25 21:09:12 -0400

    Updates

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/mvp-theoryofgreen/devprecache-manifest.bd178d54449b7104e87951bd5af7991a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/Spring-2022-CISC374/mvp-theoryofgreen/devprecache-manifest.f8dc9a3765911f4ce9ee8c165ee3f3f6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
