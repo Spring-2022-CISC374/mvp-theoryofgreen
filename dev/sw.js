4cb24e19eb51bf94c15c3b13da11737f29283264
cthacker@udel.edu <cthacker@udel.edu>
2022-05-01 00:43:11 -0400

    Updates

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/Spring-2022-CISC374/mvp-theoryofgreen/devprecache-manifest.a5efae652195d4d15e394e2e0c93b215.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/Spring-2022-CISC374/mvp-theoryofgreen/devprecache-manifest.a66a1d37d39ba6ada83dd44b20b19d1e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
