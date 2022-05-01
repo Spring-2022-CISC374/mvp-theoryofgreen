1724ee3b1e08d93de5fca27d6d4af453068e4c8b
cthacker@udel.edu <cthacker@udel.edu>
2022-05-01 02:11:39 -0400

    Updates

---
+++ alpha/precache-manifest.7acf50d8963d5a48ecc19ebcde7d15e1.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1697664c98b17930389c4db12b5951",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha021b054b7c7d3b8b8daec0dc4b01acee.png"
+  },
+  {
+    "revision": "0d10003f3e3ae21855b6a15d477a5844",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha15807951173f4e6d77158a0b6d4254c4.png"
+  },
+  {
+    "revision": "d115483caa9e9a21aead2978140eece0",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha178cbefce172d8e303a22c3d61710aea.png"
+  },
+  {
+    "revision": "2285a038362a25f5084a10213e57925c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha1e778c592c1a3796d1fa97c964c4c154.png"
+  },
+  {
+    "revision": "58c9ada9c6a53d3bc847b34f5d516a7c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha2f725f616682839c0af76bab32bb9830.png"
+  },
+  {
+    "revision": "a48c3ab80b1185a241c4b4b8c0b3be20",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha37f16d745b075bb2a248e461a3c102dc.png"
+  },
+  {
+    "revision": "7a9fd18fb9435d19754dcb651032056d",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha3b819b64c00930d4d945c2f0cb68ec13.png"
+  },
+  {
+    "revision": "e540f4723209e0f945944902550a6458",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha3cf0bd5272af6e63d788ac9211b6bcde.png"
+  },
+  {
+    "revision": "387e5cee417f2308b9d6e90e8d00413b",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha417f2051c46b9fb1ef35901071b33e70.png"
+  },
+  {
+    "revision": "cc2c768a5361edee983ecc7718ad91b9",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha430a4f6005b5f827d5d27b6bd7aaa765.png"
+  },
+  {
+    "revision": "49e35a7964a0856da923c74b57845bbc",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha4a393edfc09a0b2a75a55b6a6a8dd0e7.png"
+  },
+  {
+    "revision": "ff0277ade37430322635d271888195b3",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha556540447f89c2caf5a28bc1a25c9014.png"
+  },
+  {
+    "revision": "8830a68b13d938a6ee767a2f5a6660aa",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha667cdbfe4efb6d5aca72174cea5079cb.png"
+  },
+  {
+    "revision": "9ff8f2df950bcebf7b9354f9837dfe8c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha67701efdf3757c8084877d035362dac3.png"
+  },
+  {
+    "revision": "7b5a920145a6c3027a1ff5f555ad45dd",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha82a9f5abd76ca1cf5f9ded51f8175448.png"
+  },
+  {
+    "revision": "6bd644f02a3ff9fc47d8c23af7d5a882",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha84e422d29841122d752c3c96aaa72ebc.png"
+  },
+  {
+    "revision": "36bfa505e3549685ecd8672b62aa2afb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha8a7f7cc730a7e10fdba480049caee1c5.png"
+  },
+  {
+    "revision": "083efc1dda8fd4286aefbdc79bc11a97",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alpha930857bffa85c6defad2c2ad28819dd2.png"
+  },
+  {
+    "revision": "6a5fc3ccbc8f16719b4b4c05de14be45",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/.DS_Store"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "501415717ab729077ef1195712d1a157",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/plants/trees.png"
+  },
+  {
+    "revision": "2b5968c4544a77b36c8a33f4b97aab5c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/.DS_Store"
+  },
+  {
+    "revision": "34febb06a1f20f86e6500723c5175ef1",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/bandage.png"
+  },
+  {
+    "revision": "989783388b700f1020258a1a93f85641",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/bear.png"
+  },
+  {
+    "revision": "7b5a920145a6c3027a1ff5f555ad45dd",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/campfire.png"
+  },
+  {
+    "revision": "ae016e73d7b9ef5750b069d04aa0e6e5",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/chicken.png"
+  },
+  {
+    "revision": "937da18e19cfe2473278f1b2fea0ebba",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/food/PC Computer - Stardew Valley - Food.png"
+  },
+  {
+    "revision": "ff0277ade37430322635d271888195b3",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/food/meat.png"
+  },
+  {
+    "revision": "083efc1dda8fd4286aefbdc79bc11a97",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/pig/pig-backfacing.png"
+  },
+  {
+    "revision": "c093ccdb1f9075d628cfbdb4989d44a3",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/pig/pig-frontfacing.png"
+  },
+  {
+    "revision": "7aed8eebe588db7f1f4aa02c5013224b",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/pig/pig.png"
+  },
+  {
+    "revision": "9ff8f2df950bcebf7b9354f9837dfe8c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/attack.png"
+  },
+  {
+    "revision": "c1f3c133740da92cd13b74553f504ee6",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/attack_left.png"
+  },
+  {
+    "revision": "0d10003f3e3ae21855b6a15d477a5844",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/backidle.png"
+  },
+  {
+    "revision": "276cd9c103c97784e1bf4ab8a8fcfa7d",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/character.png"
+  },
+  {
+    "revision": "9bd7ce263c7352004f19aaa6264b30cb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_down_left.png"
+  },
+  {
+    "revision": "e540f4723209e0f945944902550a6458",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_down_left_idle.png"
+  },
+  {
+    "revision": "cc2c768a5361edee983ecc7718ad91b9",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_down_right.png"
+  },
+  {
+    "revision": "49ac1741a59f4f72fde082c8d8b7bf9f",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_down_right_idle.png"
+  },
+  {
+    "revision": "522ea3e5b1ab1531749c0639fd24b8cb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_up_left.png"
+  },
+  {
+    "revision": "387e5cee417f2308b9d6e90e8d00413b",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_up_left_idle.png"
+  },
+  {
+    "revision": "36bfa505e3549685ecd8672b62aa2afb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_up_right.png"
+  },
+  {
+    "revision": "b65ca20b5d42478d57f14361cad5664e",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/diag_up_right_idle.png"
+  },
+  {
+    "revision": "58c9ada9c6a53d3bc847b34f5d516a7c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/downwalk.png"
+  },
+  {
+    "revision": "d115483caa9e9a21aead2978140eece0",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/frontidle.png"
+  },
+  {
+    "revision": "7a9fd18fb9435d19754dcb651032056d",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/leftidle.png"
+  },
+  {
+    "revision": "c358c0910b91fab8257acfcc4dd10382",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/leftwalk.png"
+  },
+  {
+    "revision": "48bf03e5375a8e709e73d926c5b47e93",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/main-character.png"
+  },
+  {
+    "revision": "c8efcdcd7cb1067d2e95dd59a95e0beb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/main-character_emo.png"
+  },
+  {
+    "revision": "49e35a7964a0856da923c74b57845bbc",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/rightidle.png"
+  },
+  {
+    "revision": "a48c3ab80b1185a241c4b4b8c0b3be20",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/rightwalk.png"
+  },
+  {
+    "revision": "6bd644f02a3ff9fc47d8c23af7d5a882",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/player/upwalk.png"
+  },
+  {
+    "revision": "2285a038362a25f5084a10213e57925c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/rock.png"
+  },
+  {
+    "revision": "5e2b4fe8cdb535de4e6aa715fc73a4b8",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/shelter.png"
+  },
+  {
+    "revision": "4d1697664c98b17930389c4db12b5951",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/weeds.png"
+  },
+  {
+    "revision": "fa93d6241e6c7c85f559af4161ca067d",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/wolf.png"
+  },
+  {
+    "revision": "8830a68b13d938a6ee767a2f5a6660aa",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaassets/sprites/wood.png"
+  },
+  {
+    "revision": "522ea3e5b1ab1531749c0639fd24b8cb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphab7a5e76a1fe334c9443d5cf4586f9ca1.png"
+  },
+  {
+    "revision": "9bd7ce263c7352004f19aaa6264b30cb",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphacf07ba93fb8a942109137089977c7c0a.png"
+  },
+  {
+    "revision": "49ac1741a59f4f72fde082c8d8b7bf9f",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphad204bf9f3be03d0be8bc1c2d6aedce5e.png"
+  },
+  {
+    "revision": "b65ca20b5d42478d57f14361cad5664e",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphad8c29f32148f4286f2f47c32d1ca715f.png"
+  },
+  {
+    "revision": "5e2b4fe8cdb535de4e6aa715fc73a4b8",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaedf8925a8a4d4acd3627698aca8fb3bf.png"
+  },
+  {
+    "revision": "c093ccdb1f9075d628cfbdb4989d44a3",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaee536e3ff2d36ff24b5c16b3bf6159ef.png"
+  },
+  {
+    "revision": "34febb06a1f20f86e6500723c5175ef1",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaf260cab933a7d37739290adb1e7d6569.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphafavicon.ico"
+  },
+  {
+    "revision": "c1f3c133740da92cd13b74553f504ee6",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphafceda7737f00f7cfbcdcb9344c33ae6c.png"
+  },
+  {
+    "revision": "c358c0910b91fab8257acfcc4dd10382",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphafd1fe502cba94731dee3ae6798248a86.png"
+  },
+  {
+    "revision": "26dac8b34c2f73cf54da",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphagen.main.6aac36566078493aeefe.bundle.js"
+  },
+  {
+    "revision": "0b0c0e19f655c6df11ec",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphagen.vendors.ebc8eab34d8639d35771.bundle.js"
+  },
+  {
+    "revision": "e9cb70698ae62bde7fc0050117e9e94c",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphaindex.html"
+  },
+  {
+    "revision": "e0348f88b600d840ce69766857cc2261",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphamanifest.json"
+  },
+  {
+    "revision": "725810e9ba22a8922c7a294e5a3f9c58",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphascenes/game.js"
+  },
+  {
+    "revision": "5c62495a52b57e3b002d8dca78ffabc9",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphascenes/phaser.min.js"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/Spring-2022-CISC374/mvp-theoryofgreen/alphasw.js"
+  }
+]);
